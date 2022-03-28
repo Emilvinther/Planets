5768ac184af3f03dd418f49724cb3af858f5e169
Emil Lorentzen <emilerher@gmail.com>
2022-03-28 14:00:14 +0200

    Add project files.

---
+++ Planets/Planet.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    public class Planet
+    {
+        private string planetname;
+        private double mass;
+        private int diameter;
+        private int density;
+        private double gravity;
+        private double rotationperiod;
+        private double lengthofdays;
+        private double distancefromsun;
+        private double obitalperiod;
+        private double obitalvelocity;
+        private int temperature;
+        private int numberofmoons;
+        private bool ringsystem = false;
+
+        public string Planetname { get => planetname; set => planetname = value; }
+        public double Mass { get => mass; set => mass = value; }
+        public int Diameter { get => diameter; set => diameter = value; }
+        public int Density { get => density; set => density = value; }
+        public double Gravity { get => gravity; set => gravity = value; }
+        public double Rotationperiod { get => rotationperiod; set => rotationperiod = value; }
+        public double Lengthofdays { get => lengthofdays; set => lengthofdays = value; }
+        public double Distancefromsun { get => distancefromsun; set => distancefromsun = value; }
+        public double Obitalperiod { get => obitalperiod; set => obitalperiod = value; }
+        public double Obitalvelocity { get => obitalvelocity; set => obitalvelocity = value; }
+        public int Temperature { get => temperature; set => temperature = value; }
+        public int Numberofmoons { get => numberofmoons; set => numberofmoons = value; }
+        public bool Ringsystem { get => ringsystem; set => ringsystem = value; }
+
+        public Planet(string planetname, double mass, int diameter, int density, double gravity, double rotationperiod, double lengthofdays, double distancefromsun, double obitalperiod, double obitalvelocity, int temperature, int numberofmoons, bool ringsystem)
+        {
+            Planetname = planetname;
+            Mass = mass;
+            Diameter = diameter;
+            Density = density;
+            Gravity = gravity;
+            Rotationperiod = rotationperiod;
+            Lengthofdays = lengthofdays;
+            Distancefromsun = distancefromsun;
+            Obitalperiod = obitalperiod;
+            Obitalvelocity = obitalvelocity;
+            Temperature = temperature;
+            Numberofmoons = numberofmoons;
+            Ringsystem = ringsystem;
+        }
+
+        public override string ToString()
+        {
+            return $"\nPlanet properties\n\n" +
+            $"\nPlanet Name = {Planetname}" +
+            $"\nPlanet Mass = {Mass}" +
+            $"\nPlanet Diameter = {Diameter}" +
+            $"\nPlanet Density = {Density}" +
+            $"\nPlanet Gravity = {Gravity}" +
+            $"\nPlanet Rotation Period = {Rotationperiod}" +
+            $"\nPlanet Day Length = {Lengthofdays}" +
+            $"\nPlanet Distance from sun = {Distancefromsun}" +
+            $"\nPlanet Obital Period = {Obitalperiod}" +
+            $"\nPlanet Obital Velocity = {Obitalvelocity}" +
+            $"\nPlanets Medi Temperature = {Temperature}" +
+            $"\nNumber of moons orbiting the planet {Numberofmoons}" +
+            $"\nDoes it have a ringsystem {(Ringsystem == true ? "Yes" : "No")}";
+        }
+    }
+}
---
+++ Planets/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Planets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet mercury = new Planet("Mercury",
+                0.300,
+                4879,
+                5427,
+                3.7,
+                1407.6,
+                4222.6,
+                57.9,
+                88.0,
+                47.4,
+                167,
+                0,
+                false
+                );
+
+            Planet venus = new Planet("Venus",
+                4.87,
+                12104,
+                5243,
+                8.9,
+                5832.5,
+                2802.0,
+                108.2,
+                224.7,
+                35.0,
+                464,
+                0,
+                false
+                );
+
+            Planet earth = new Planet("Earth",
+                5.97,
+                12756,
+                5514,
+                9.8,
+                23.9,
+                24.0,
+                149.6,
+                365.2,
+                29.8,
+                15,
+                1,
+                false
+                );
+
+            Planet mars = new Planet("Mars",
+                0.642,
+                6792,
+                3933,
+                3.7,
+                24.6,
+                24.7,
+                227.9,
+                687.0,
+                24.1,
+                -65,
+                2,
+                false
+                );
+
+            Planet jupiter = new Planet("Jupiter",
+                1898,
+                142984,
+                1326,
+                23.1,
+                9.9,
+                9.9,
+                778.6,
+                4331,
+                13.1,
+                -110,
+                67,
+                true
+                );
+
+            Planet saturn = new Planet("Saturn",
+                568,
+                120536,
+                687,
+                9.0,
+                10.7,
+                10.7,
+                1433.5,
+                10747,
+                9.7,
+                -140,
+                62,
+                true
+                );
+
+            Planet uranus = new Planet("Uranus",
+                86.8,
+                51118,
+                1271,
+                8.7,
+                17.2,
+                17.2,
+                2872.5,
+                30589,
+                6.8,
+                -195,
+                27,
+                true
+                );
+
+            Planet neptune = new Planet("Neptune",
+                102,
+                49528,
+                1638,
+                11.0,
+                16.1,
+                16.1,
+                4495.1,
+                59.8,
+                5.4,
+                -200,
+                14,
+                true
+                );
+
+            Planet pluto = new Planet("Pluto",
+               0.0146,
+               2370,
+               2095,
+               0.7,
+               153.3,
+               153.3,
+               5904.4,
+               90.56,
+               4.7,
+               -225,
+               5,
+               false
+               );
+
+            List<Planet> planets = new List<Planet>();
+            planets.Add(mercury);
+            planets.Add(earth);
+            planets.Add(mars);
+            planets.Add(jupiter);
+            planets.Add(saturn);
+            planets.Add(uranus);
+            planets.Add(neptune);
+            planets.Add(pluto);
+            planets.Insert(1, venus);
+            planets.Remove(pluto);
+            planets.Add(pluto);
+            Console.WriteLine(planets.Count);
+
+
+
+            foreach (Planet i in planets)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Planet> belowzeros = planets.Where(planets => planets.Temperature < 0).ToList();
+
+            foreach (Planet i in belowzeros)
+            {
+                Console.WriteLine(i.Temperature);
+            }
+
+            List<Planet> planetdiamenter = planets.Where(planets => planets.Diameter > 10000 && planets.Diameter < 50000).ToList();
+
+            foreach (Planet i in planetdiamenter)
+            {
+                Console.WriteLine(i.Diameter);
+            }
+
+            planets.Clear();
+            
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
